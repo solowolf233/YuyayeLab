3021ce15e549b4499f221e8c956d3d45c66e5bf2
solowolf233 <swfeng@dlmu.edu.cn>
2016-08-11 20:44:06 +0800

    database password change

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -16,7 +16,7 @@ $db_config = array(
     'DB_HOST'   => '127.0.0.1',       // 服务器地址
     'DB_NAME'   => 'yuyayelab',       // 数据库名
     'DB_USER'   => 'root',            // 用户名
-    'DB_PWD'    => '123456',                // 密码
+    'DB_PWD'    => 'cicle123',                // 密码
     'DB_PORT'   => '3306',            // 端口
     'DB_PREFIX' => 'ddf_',           // 数据库表前缀
         );
